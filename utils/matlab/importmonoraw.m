66e70e5e58c1296935851d8b95ad359983c02b50
Nicolas Bruot <coremeltdown@bruot.org>
2018-03-08 17:29:34 +0900

    Adding single frame import feature in importmonoraw.m

--- utils/matlab/importmonoraw.m
+++ utils/matlab/importmonoraw.m
@@ -1,12 +1,15 @@
-function [data, timestamps] = importmonoraw(path)
+function [data, timestamps] = importmonoraw(path, frame)
 % IMPORTMONORAW  Loads a .raw/.rawm movie into a 3d array.
 %   data = IMPORTMONORAW(path) Loads a movie in data.
-%   [data, timestamp] = IMPORTMONORAW(path) Loads a movie in data and timestamps vector.
+%   [data, timestamp] = IMPORTMONORAW(path [, frame]) Loads a movie in data and timestamps vector.
+%
+% If the frame index "frame" is given, returns "data" and "timestamp" for
+% that specific frame without reading the whole .raw file.
 
 % This file is part of the xiFastMovie software, a movie recorder for Ximea
 % cameras.
 %
-% Copyright 2017 Nicolas Bruot
+% Copyright 2017, 2018 Nicolas Bruot
 %
 %
 % xiFastMovie is free software: you can redistribute it and/or modify
@@ -22,6 +25,16 @@ function [data, timestamps] = importmonoraw(path)
 % You should have received a copy of the GNU General Public License
 % along with xiFastMovie.  If not, see <http://www.gnu.org/licenses/>.
 
+if nargin == 2
+    single_frame = 1;
+    if ~isnumeric(frame) || ~isequal(size(frame), [1, 1])
+        throw(MException('Rawm:TypeError', ...
+            'The "frame" argument must be an integer.'));
+    end
+else
+    single_frame = 0;
+end
+
 tree = xmlread(path);
 root = tree.getElementsByTagName('movie_metadata').item(0);
 header = root.getElementsByTagName('header').item(0);
@@ -32,9 +45,11 @@ height = str2double(get_elem_value(header, 'height'));
 pixel_fmt = char(get_elem_value(header, 'pixel_format'));
 if strcmp(pixel_fmt, 'Mono8')
     precision = 'uint8=>uint8';
+    bytes_per_sample = 1;
 elseif strcmp(pixel_fmt, 'Mono10') || strcmp(pixel_fmt, 'Mono12') ...
     || strcmp(pixel_fmt, 'Mono14') || strcmp(pixel_fmt, 'Mono16')
     precision = 'uint16=>uint16';
+    bytes_per_sample = 2;
 else
     throw(MException('Rawm:ValueError', ...
         'Pixel format "%s" not recognized.', pixel_fmt));
@@ -53,27 +68,55 @@ end
 frames_tree = root.getElementsByTagName('frames').item(0);
 frames = frames_tree.getElementsByTagName('frame');
 n_frames = frames.getLength;
-
-if nargout > 1
-    timestamps = zeros(n_frames, 1);
-    for i = 1:n_frames
-        timestamps(i) = str2double( ...
-            frames.item(i - 1).getAttribute('timestamp'));
+if single_frame
+    if frame < 1 || frame > n_frames
+        throw(MException('Rawm:ValueError', ...
+            '"frame" is out of range.'));
     end
 end
 
+if nargout > 1
+    if single_frame
+        timestamps = str2double( ...
+            frames.item(frame - 1).getAttribute('timestamp'));
+    else
+        timestamps = zeros(n_frames, 1);
+        for i = 1:n_frames
+            timestamps(i) = str2double( ...
+                frames.item(i - 1).getAttribute('timestamp'));
+        end
+    end
+end
+
+[basedir, filename, ~] = fileparts(path);
+raw_path = fullfile(basedir, strcat(filename, '.raw'));
+
+% Check .raw file size
+stats = dir(raw_path);
+if stats.bytes ~= width * height * n_frames * bytes_per_sample
+    throw(MException('Rawm:FileError', ...
+        '.raw file has wrong size.'));
+end
+
 % Read .raw file
-[dir, filename, ~] = fileparts(path);
-raw_path = fullfile(dir, strcat(filename, '.raw'));
 fid = fopen(raw_path, 'rb');
-if big_endian
-    data = fread(fid, width * height * n_frames, precision, 'ieee-be');
+if single_frame
+    fseek(fid, width * height * (frame - 1) * bytes_per_sample, 'bof');
+    if big_endian
+        data = fread(fid, width * height, precision, 'ieee-be');
+    else
+        data = fread(fid, width * height, precision);
+    end
+    data = reshape(data, [width, height]);
 else
-    data = fread(fid, width * height * n_frames, precision);
+    if big_endian
+        data = fread(fid, width * height * n_frames, precision, 'ieee-be');
+    else
+        data = fread(fid, width * height * n_frames, precision);
+    end
+    data = reshape(data, [width, height, n_frames]);
 end
 fclose(fid);
-
-data = reshape(data, [width, height, n_frames]);
 
 end
 
